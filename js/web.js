2ff2ce848241bd1d3e5e08cb83417a0ec890c944
pangyuchoi <vksrbakstp@naver.com>
2022-03-19 12:08:35 +0900

    변수와타입

---
+++ js/web.js
@@ -0,0 +1,14 @@
+var count= 100;
+count += 50;//숫자 타입의 변수
+count += "원";//문자 타입으로 변경
+count +=50;//문자로 계속살아갈수밖에
+count =100;//150원 50100
+var count = "100원";
+var arr=["회사소개","제품소개","고객센터"];
+
+//number,string, null undefinded, Object
+
+console.log("count 변수값은?",count, "데이터 타입은 ", typeof count);
+console.log("arr 값을 가져올때",arr[0], "데이터 타입은 ", typeof arr);
+
+// count 변수값은? ... go 값은 고릴라?
